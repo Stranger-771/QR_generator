99647c12469aff0d52b66b5031e98f2156e2b9a6
Stranger-771 <gauravpandey.gp9@gmail.com>
2023-02-11 11:39:48 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,41 @@
+import QRCode from 'qrcode'
 import { useState } from 'react'
-import { QRCode } from 'qrcode'
-
 
 function App() {
-  const [url, setUrl] = useState('')
-  const [qrcode, setQrcode] = useState('')
+	const [url, setUrl] = useState('')
+	const [qr, setQr] = useState('')
 
-  const GenerateQRCode = () => {
-    QRCode.toDataURL(url, (err,url) =>{
-      if (err) return console.error(err)
+	const GenerateQRCode = () => {
+		QRCode.toDataURL(url, {
+			width: 800,
+			margin: 2,
+			color: {
+				dark: '#335383FF',
+				light: '#EEEEEEFF'
+			}
+		}, (err, url) => {
+			if (err) return console.error(err)
 
-      console.log(url)
-      setQrcode(url)
-    })
-  }
+			console.log(url)
+			setQr(url)
+		})
+	}
 
-  return (
-    <div className="App">
-      <h1>QR code Generator</h1>
-      
-      <input type="text" 
-      placeholder="e.g. https://google.com"
-      value={url}
-      onChange={(evt) => setUrl(evt.target.value)}/>
-      
-
-      <button onClick={GenerateQRCode}>Generate</button>
-      <img src={qrcode} />
-    </div>
-  )
+	return (
+		<div className="app">
+			<h1>QR Generator</h1>
+			<input 
+				type="text"
+				placeholder="e.g. https://google.com"
+				value={url}
+				onChange={e => setUrl(e.target.value)} />
+			<button onClick={GenerateQRCode}>Generate</button>
+			{qr && <>
+				<img src={qr} />
+				<a href={qr} download="qrcode.png">Download</a>
+			</>}
+		</div>
+	)
 }
 
 export default App
